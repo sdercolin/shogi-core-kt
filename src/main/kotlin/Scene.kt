e2c00ce8eef60402c32e8fedf974164c7ff7f670
weng.yitong <weng.yitong@linecorp.com>
2019-09-19 12:17:39 +0900

    Add Scene.initial

--- src/main/kotlin/Scene.kt
+++ src/main/kotlin/Scene.kt
@@ -62,5 +62,51 @@ data class Scene constructor(val pieces: List<Piece>) {
     companion object {
         val empty: Scene
             get() = Scene(listOf())
+
+        val initial: Scene
+            get() = Scene(
+                listOf(
+                    King(Color.BLACK, Color.BLACK, Position(4, 8), 0),
+                    Gold(Color.BLACK, Color.BLACK, Position(3, 8), 1),
+                    Gold(Color.BLACK, Color.BLACK, Position(5, 8), 2),
+                    Silver(Color.BLACK, Color.BLACK, Position(2, 8), 3),
+                    Silver(Color.BLACK, Color.BLACK, Position(6, 8), 4),
+                    Knight(Color.BLACK, Color.BLACK, Position(1, 8), 5),
+                    Knight(Color.BLACK, Color.BLACK, Position(7, 8), 6),
+                    Lance(Color.BLACK, Color.BLACK, Position(0, 8), 7),
+                    Lance(Color.BLACK, Color.BLACK, Position(8, 8), 8),
+                    Rook(Color.BLACK, Color.BLACK, Position(1, 7), 9),
+                    Bishop(Color.BLACK, Color.BLACK, Position(7, 7), 10),
+                    Pawn(Color.BLACK, Color.BLACK, Position(0, 6), 11),
+                    Pawn(Color.BLACK, Color.BLACK, Position(1, 6), 12),
+                    Pawn(Color.BLACK, Color.BLACK, Position(2, 6), 13),
+                    Pawn(Color.BLACK, Color.BLACK, Position(3, 6), 14),
+                    Pawn(Color.BLACK, Color.BLACK, Position(4, 6), 15),
+                    Pawn(Color.BLACK, Color.BLACK, Position(5, 6), 16),
+                    Pawn(Color.BLACK, Color.BLACK, Position(6, 6), 17),
+                    Pawn(Color.BLACK, Color.BLACK, Position(7, 6), 18),
+                    Pawn(Color.BLACK, Color.BLACK, Position(8, 6), 19),
+                    King(Color.WHITE, Color.WHITE, Position(4, 0), 20),
+                    Gold(Color.WHITE, Color.WHITE, Position(3, 0), 21),
+                    Gold(Color.WHITE, Color.WHITE, Position(5, 0), 22),
+                    Silver(Color.WHITE, Color.WHITE, Position(2, 0), 23),
+                    Silver(Color.WHITE, Color.WHITE, Position(6, 0), 24),
+                    Knight(Color.WHITE, Color.WHITE, Position(1, 0), 25),
+                    Knight(Color.WHITE, Color.WHITE, Position(7, 0), 26),
+                    Lance(Color.WHITE, Color.WHITE, Position(0, 0), 27),
+                    Lance(Color.WHITE, Color.WHITE, Position(8, 0), 28),
+                    Rook(Color.WHITE, Color.WHITE, Position(7, 1), 29),
+                    Bishop(Color.WHITE, Color.WHITE, Position(1, 1), 30),
+                    Pawn(Color.WHITE, Color.WHITE, Position(0, 2), 31),
+                    Pawn(Color.WHITE, Color.WHITE, Position(1, 2), 32),
+                    Pawn(Color.WHITE, Color.WHITE, Position(2, 2), 33),
+                    Pawn(Color.WHITE, Color.WHITE, Position(3, 2), 34),
+                    Pawn(Color.WHITE, Color.WHITE, Position(4, 2), 35),
+                    Pawn(Color.WHITE, Color.WHITE, Position(5, 2), 36),
+                    Pawn(Color.WHITE, Color.WHITE, Position(6, 2), 37),
+                    Pawn(Color.WHITE, Color.WHITE, Position(7, 2), 38),
+                    Pawn(Color.WHITE, Color.WHITE, Position(8, 2), 39)
+                )
+            )
     }
 }
